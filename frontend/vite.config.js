576c668bfcef43b75ddce98d317e3653fcafbc8d
Anirudh77177 <151743579+Anirudh77177@users.noreply.github.com>
2026-04-24 02:30:35 +0530

    Update vite.config.js
    Enable ngrok links in Vite configuration to prevent errors on link changes.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    // 🔥 ERROR FIX: Ye line Vite ko ngrok links allow karne degi
-    allowedHosts: true, // true likhne se baar-baar link change hone pe error nahi aayega
+    allowedHosts: true, 
     
     proxy: {
       '/api': {
